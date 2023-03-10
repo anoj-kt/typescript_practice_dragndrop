86a3ebff89e8bb05f577b362f0979ed995eab5cf
anoj-kt <anojk28@gmail.com>
2023-03-10 12:24:55 +0100

    Created ProjectList class. Added methods to above class: 1. renderContent: Renders heading and creates id for ul element. 2. attach: Attaches element to the DOM

--- src/app.ts
+++ src/app.ts
@@ -55,6 +55,38 @@ function Autobind(_: any, _2: string, descriptor: PropertyDescriptor) {
   return adjustedMethod;
 }
 
+// Project List class
+class ProjectList {
+  templateElement: HTMLTemplateElement;
+  hostElement: HTMLDivElement;
+  element: HTMLElement;
+
+  constructor(private type: 'active' | 'finished') {
+    this.templateElement = document.getElementById(
+      'project-list'
+    )! as HTMLTemplateElement;
+    this.hostElement = document.getElementById('app')! as HTMLDivElement;
+    const importedNode = document.importNode(
+      this.templateElement.content,
+      true
+    );
+    this.element = importedNode.firstElementChild as HTMLElement;
+    this.element.id = `${this.type}-projects`;
+
+    this.renderContent();
+    this.attach();
+  }
+
+  private renderContent() {
+    this.element.querySelector('h2')!.textContent = `${this.type.toUpperCase()} PROJECTS`;
+    this.element.querySelector('ul')!.id = `${this.type}-projects`;
+  }
+
+  private attach() {
+    this.hostElement.insertAdjacentElement('beforeend', this.element);
+  }
+}
+
 // Project Input class
 class ProjectInput {
   templateElement: HTMLTemplateElement;
@@ -148,3 +180,5 @@ class ProjectInput {
 }
 
 const prj = new ProjectInput();
+const active = new ProjectList('active')
+const finished = new ProjectList('finished')
